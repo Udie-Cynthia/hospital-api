41bab8bc814dccc5321a8c183c30f4070b5e4366
Udie Cynthia <udiecynthia@gmail.com>
2025-10-07 21:09:37 +0100

    feat: hospital API with real photo uploads

--- db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///hospital.sqlite3")
-kwargs = {"connect_args": {"check_same_thread": False}} if DATABASE_URL.startswith("sqlite") else {}
-engine = create_engine(DATABASE_URL, **kwargs)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base = declarative_base()
